50c3c3acceea1ff61cf58485f38025ce7226be6e
Alex <134059971+w1lldy@users.noreply.github.com>
2023-10-11 07:29:40 +0000

    игра в столицы #с костылями

--- capitals.py
+++ capitals.py
@@ -1,23 +1,20 @@
 # знаю, что по-уродски выглядит, заметки для будущего себя:
-# 1) избавься от множества break
+# 1) избавься от множества break UPD:от флага избавился
 # 2) возможна ли рандомизация 1-ых значений словарей без преобразования в список (модуль рандом вроде не работает со словарями)
-# 3) 
-
+# 3) другую чушь вроде почистил
 import random
 capitals_dict = {'Alabama': 'Montgomery', 'Alaska' : 'Juneau', 'Arizona': 'Phoenix', 'Arkansas': 'Little Rock',
 'California': 'Sacramento', 'Colorado': 'Denver', 'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida': 'Tallahassee',
 'Georgia': 'Atlanta',}
 random_capital = []
-flag = 1
 for capital, state in capitals_dict.items(): # фу костыль
     random_capital.append(capital)
 RC = random.choice(random_capital)
-print(f"Input state of {RC}")
-answer = input().lower()
-while (flag != 0): # вообще коляска
+answer = input(f"Input state of {RC}\n").lower()
+while(answer): # вообще коляска
     if (answer == "exit"):
-        print("Goodbye"); flag = 1; break
-    elif (answer == "test"):
-        print('Correct!'); flag = 1; break
+        print("Goodbye"); break
+    elif (answer == capitals_dict[RC].lower()):
+        print('Correct!'); break
     else:
         print('Incorrect! Try again!'); answer = input().lower()
